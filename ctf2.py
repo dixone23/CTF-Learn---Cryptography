b92b0318661e823fda1d7c59057fd7b595b2be0b
Marcin Kucharski <dixone23@gmail.com>
2020-09-03 20:54:55 +0200

    Flag for https://ctflearn.com/challenge/158

--- ctf2.py
+++ ctf2.py
@@ -1,8 +1,6 @@
 string = '0xc4115 0x4cf8'
 
-def decode(s):
-    for elem in s.split(' '):
-        d = int(elem, 16)
-        print(chr(d), d)
+A = int(string.split(' ')[0], 16)
+B = int(string.split(' ')[1], 16)
 
-decode(string)
+print(hex(A ^ B))
